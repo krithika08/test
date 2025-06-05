ea301a19700952f61712d7dd89af86e65d9d094a
krithika08 <79592558+krithika08@users.noreply.github.com>
2025-06-05 13:22:04 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def divide(a, b):
+    return a / b  # ⚠️ will raise ZeroDivisionError if b = 0
+
+if __name__ == "__main__":
+    print("Add: ", add(2, 3))
+    print("Divide: ", divide(4, 2))
