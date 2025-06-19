a75545c9a6bc2197d4a72c0f2ea842eee8d2e18a
krithika08 <79592558+krithika08@users.noreply.github.com>
2025-06-19 12:45:24 +0530

    Fix bug in test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-def get_primes_up_to(limit):
-    primes = []
-    for num in range(2, limit + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
+def main():\n print('Hello')
